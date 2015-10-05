9dc418a5cfbea7dbe0347ea78121e2859d3c31fc
LIU Yu <liuyu@arena.localdomain>
2015-10-05 23:12:59 +0000

    Merge pull request #3 from liuyu81/master
    Preview Release of DataGator Gephi Plugins (RC3)

--- DataGatorMatrixImporter/src/org/datagator/ext/gephi/importer/MatrixJsonImporterUI.java
+++ DataGatorMatrixImporter/src/org/datagator/ext/gephi/importer/MatrixJsonImporterUI.java
@@ -57,9 +57,10 @@ import org.openide.util.lookup.ServiceProvider;
 
 /**
  * Main UI logic implementation.
+ *
  * This is the representation layer of the importer responsible for mapping UI
  * input / output to business logic settings / outcomes.
- * 
+ *
  * @author LIU Yu <liuyu@opencps.net>
  */
 @ServiceProvider(service = ImporterUI.class)
@@ -67,7 +68,7 @@ public class MatrixJsonImporterUI
     implements ImporterUI
 {
 
-    private static Map<String, MatrixJsonImporter.ColumnRoleType> roleDict;
+    private static final Map<String, MatrixJsonImporter.ColumnRoleType> roleDict;
 
     static {
         roleDict = new HashMap<String, MatrixJsonImporter.ColumnRoleType>();
@@ -76,11 +77,11 @@ public class MatrixJsonImporterUI
         roleDict.put(MatrixJsonImporterUIPanel.TARGET_NODE,
             MatrixJsonImporter.ColumnRoleType.TARGET_NODE);
         roleDict.put(MatrixJsonImporterUIPanel.NODE,
-            MatrixJsonImporter.ColumnRoleType.UNDIRECTED_NODE);
+            MatrixJsonImporter.ColumnRoleType.NODE);
         roleDict.put(MatrixJsonImporterUIPanel.EDGE_WEIGHT,
             MatrixJsonImporter.ColumnRoleType.EDGE_WEIGHT);
-        // roleDict.put(MatrixJsonImporterUIPanel.EDGE_LABEL,
-        //     MatrixJsonImporter.ColumnRoleType.EDGE_LABEL);
+        roleDict.put(MatrixJsonImporterUIPanel.EDGE_LABEL,
+            MatrixJsonImporter.ColumnRoleType.EDGE_LABEL);
         roleDict.put(MatrixJsonImporterUIPanel.TIME,
             MatrixJsonImporter.ColumnRoleType.TIME);
     }
@@ -101,6 +102,9 @@ public class MatrixJsonImporterUI
             throw new RuntimeException(ex);
         }
 
+        final MatrixJsonImporterUIPanel thePanel = this.panel;
+        final MatrixJsonImporter theImporter = this.importer;
+
         // pre-fetch matrix columns for role annotation, for large files (i.e.
         // > 1 mil records), this may take more than 10 seconds, so we need
         // to run in an asynchronous task to avoid jamming the UI.
@@ -109,12 +113,12 @@ public class MatrixJsonImporterUI
             @Override
             public void run()
             {
-                Cursor oldCursor = panel.getCursor();
-                panel.setCursor(new Cursor(Cursor.WAIT_CURSOR));
-                Matrix preview = importer.getMatrixHeaders();
-                panel.updateTableModel(preview.getRowsCount(),
+                Cursor oldCursor = thePanel.getCursor();
+                thePanel.setCursor(new Cursor(Cursor.WAIT_CURSOR));
+                Matrix preview = theImporter.getMatrixHeaders();
+                thePanel.updateTableModel(preview.getRowsCount(),
                     preview.getColumnsCount(), preview.rows());
-                panel.setCursor(oldCursor);
+                thePanel.setCursor(oldCursor);
             }
         };
         executor.execute(null, asyncTask);
@@ -139,19 +143,20 @@ public class MatrixJsonImporterUI
     @Override
     public void unsetup(boolean update)
     {
-        if (update) {            
-            // edge type: directed / undirected
+        if (update) {
+            // graph type: directed / undirected, dynamic / static
             importer.setGraphType(panel.isDirectedGraph(),
                 panel.isDynamicGraph());
-            //
+            // column roles to indices mapping
             TableModel model = panel.getTableModel();
-            for (int r = 0, c = 1; r < model.getRowCount(); r++) {
-                Object roleName = model.getValueAt(r, c);
+            for (int r = 0, c = MatrixJsonImporterUIPanel.ROLE_COL_INDEX;
+                r < model.getRowCount(); r++) {
+                String roleName = (String) model.getValueAt(r, c);
                 if (roleName == null) {
                     continue;
                 }
                 MatrixJsonImporter.ColumnRoleType roleType
-                    = this.roleDict.get(roleName);
+                    = roleDict.get(roleName);
                 if (roleType == null) {
                     importer.getReport().logIssue(new Issue(String.format(
                         NbBundle.getMessage(
